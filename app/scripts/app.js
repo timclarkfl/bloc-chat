d44d58c60ba4addf8ab6c7e1e70581a89e76a686
Tim Clark <tim.clark@outlook.com>
2017-04-22 16:14:26 -0400

    removed jquery and fixed indentation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
-(function(){
-  function config($stateProvider, $locationProvider){
-    $locationProvider
-      .html5Mode({
-        enabled: true,
-        requireBase: false
-      });
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
 
-      $stateProvider
-             .state('main', {
+        $stateProvider
+            .state('main', {
                 url: '/',
                 templateUrl: '/templates/main.html'
             });
-          }
+    }
 
-  angular
-    .module('blocChat',['ui.router', 'firebase'])
-    .config(config);
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
 })();
