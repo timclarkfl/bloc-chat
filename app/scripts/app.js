e3cb8510946ea396b6b2e5f9d28ec78c9946b792
Tim Clark <tim.clark@outlook.com>
2017-04-22 15:55:46 -0400

    checkpoint1

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+(function(){
+  function config($stateProvider, $locationProvider){
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+      $stateProvider
+             .state('main', {
+                url: '/',
+                templateUrl: '/templates/main.html'
+            });
+          }
+
+  angular
+    .module('blocChat',['ui.router', 'firebase'])
+    .config(config);
+})();
